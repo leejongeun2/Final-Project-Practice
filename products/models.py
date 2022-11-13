4873d2f05a91ccb99b72c47aa64ab009fd80097a
tmsnvl95 <tmsnvl95@naver.com>
2022-11-13 18:22:58 +0900

    model like_user 삭제

--- products/models.py
+++ products/models.py
@@ -15,4 +15,3 @@ class Product(models.Model):
     content = models.TextField()
     jjim = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name = 'jjim_product') # 한 상품은 여러명의 사용자에게 찜을 받을 수있고, 사용자는 여러 상품에 찜을 할 수 있다.
     review = models.ManyToManyField(Review, related_name = 'review_product') #  한 상품은 여러 리뷰가 있을 수 있고, 한 리뷰는 여러 상품에 있을 수 있다.   
-    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name = 'like_product') # 한 상품은 사용자에게 좋아요를 여러개 받을 수 있고, 한 사람도 여러 상품에 좋아요를 할 수 있다. 
